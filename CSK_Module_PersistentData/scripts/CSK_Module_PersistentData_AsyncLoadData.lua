17a0ac118cb32bf28ee3e277585971b139410e3c
golluroSICKAG <45095769+golluroSICKAG@users.noreply.github.com>
2025-02-04 11:11:02 +0100

    # Release 4.2.0 (#3)
    # Release 4.2.0
    
    ## New features
    - Edit values within parameters via UI
    - Trigger other modules to load specific parameters

--- CSK_Module_PersistentData/scripts/CSK_Module_PersistentData_AsyncLoadData.lua
+++ CSK_Module_PersistentData/scripts/CSK_Module_PersistentData_AsyncLoadData.lua
@@ -1,5 +1,7 @@
 ---@diagnostic disable: undefined-global, redundant-parameter, missing-parameter
 
+Script.serveEvent("CSK_PersistentData.OnNewThreadFeedbackStatus", "PersistentData_OnNewThreadFeedbackStatus")
+
 -- If App property "LuaLoadAllEngineAPI" is FALSE, use this to load and check for required APIs
 -- This can improve performance of garbage collection
 local availableAPIs = require('Configuration/PersistentData/helper/checkAPIs') -- check for available APIs
@@ -11,6 +13,17 @@ local nameOfModule = 'CSK_PersistentData'
 _G.logger = Log.SharedLogger.create('ModuleLogger')
 local json = require("Configuration.PersistentData.helper.Json")
 local scriptParams = Script.getStartArgument() -- Get parameters from model
+
+-- Timer to hide UI messages after 5 seconds
+local logTmr = Timer.create()
+logTmr:setExpirationTime(5000)
+logTmr:setPeriodic(false)
+
+--- Function to hide UI message
+local function handleOnLogTmrExpired()
+  Script.notifyEvent('PersistentData_OnNewThreadFeedbackStatus', 'EMPTY')
+end
+Timer.register(logTmr, 'OnExpired', handleOnLogTmrExpired)
 
 --- Function to trigger other modules to load their specific parameters
 ---@param data Container Data container with parameters to load.
@@ -85,3 +98,67 @@ local function handleOnNewDataToLoad(data)
 end
 Script.register('CSK_PersistentData.OnNewDataToLoad', handleOnNewDataToLoad)
 
+
+--- Function to trigger other modules to load specific parameters
+---@param moduleName string Name of module.
+---@param parameterName string Name of parameters to load.
+---@param instance? int Optional number of instance of module.
+local function handleOnNewStatusSendParametersToModule(moduleName, parameterName, instance)
+
+  local name = string.gsub(moduleName, '_1stModule_', '_')
+  name = string.gsub(name, '_Module_', '_')
+  name = string.gsub(name, '_Application_', '_')
+
+  _G.logger:fine(nameOfModule .. ": Try to set parameters " .. tostring(parameterName) .. " to module " .. tostring(name) .. " (optional instance = " .. tostring(instance) .. ").")
+  local instanceSelectionExists = false
+  local setInstanceExists = false
+  local instanceAmountExists = false
+
+  if instance then
+    instanceSelectionExists = Script.isServedAsFunction(name .. '.setSelectedInstance')
+    setInstanceExists = Script.isServedAsFunction(name .. '.setInstance')
+    instanceAmountExists = Script.isServedAsFunction(name .. '.getInstancesAmount')
+
+    if instanceAmountExists then
+      local _, amount = Script.callFunction(name .. '.getInstancesAmount')
+      if amount <= instance then
+        if instanceSelectionExists then
+          Script.callFunction(name .. '.setSelectedInstance', instance)
+        elseif setInstanceExists then
+          Script.callFunction(name .. '.setInstance', instance)
+        else
+          _G.logger:warning(nameOfModule .. ": Not possible to set instance.")
+          Script.notifyEvent('PersistentData_OnNewThreadFeedbackStatus', 'ERR')
+          logTmr:start()
+          return
+        end
+      else
+        _G.logger:warning(nameOfModule .. ": Instance does not exists.")
+        Script.notifyEvent('PersistentData_OnNewThreadFeedbackStatus', 'ERR')
+        logTmr:start()
+        return
+      end
+    end
+  end
+
+  local setParameterNameExists = Script.isServedAsFunction(name .. '.setParameterName')
+  local loadParametersExists = Script.isServedAsFunction(name .. '.loadParameters')
+
+  if setParameterNameExists and loadParametersExists then
+    Script.callFunction(name .. '.setParameterName', parameterName)
+    Script.sleep(1000)
+    local _, suc = Script.callFunction(name .. '.loadParameters')
+    if suc == nil or suc == false then
+      Script.notifyEvent('PersistentData_OnNewThreadFeedbackStatus', 'ERR')
+      logTmr:start()
+    else
+      Script.notifyEvent('PersistentData_OnNewThreadFeedbackStatus', 'OK')
+      logTmr:start()
+    end
+  else
+    _G.logger:warning(nameOfModule .. ": Not possible to load parameters.")
+    Script.notifyEvent('PersistentData_OnNewThreadFeedbackStatus', 'ERR')
+    logTmr:start()
+  end
+end
+Script.register('CSK_PersistentData.OnNewStatusSendParametersToModule', handleOnNewStatusSendParametersToModule)
